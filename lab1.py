681138046deebbb37367381ce55198c559171cec
aydansuleymanova1 <a.suleymanova@studenti.unipi.it>
2021-10-06 00:49:54 +0400

    my first commit

---
+++ lab1.py
@@ -0,0 +1,11 @@
+import calculator as c
+
+class FooCalculator:
+
+    #empty calculator
+    def __init__(self):
+        pass
+    def sum(self,m,n):
+        return c.sum(m,n)
+    def divide(self,m,n):
+        return c.divide(m,n)
